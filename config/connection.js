a1c15e48389245b6236e281ea7d8556262ebdafb
Cory Hall <ComebackCory1@gmail.com>
2022-05-08 21:18:36 -0500

    finished public js files, added handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
-const Sequelize = require("sequelize");
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+// create connection to our database, pass in your MySQL information for username and password
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -10,8 +12,8 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3006
+    port: 3306
   });
-};
+}
 
 module.exports = sequelize;
